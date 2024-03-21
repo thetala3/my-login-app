4d0496f78ae05c4bf7b5fae43b0e99639d053d88
acabestala <tala.mohammad@acabesinternational.com>
2024-03-22 01:12:29 +0300

    root navigate to login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import LoginForm from "./components/LoginForm/LoginForm";
 import ListTransaction from "./Pages/ListTransaction";
 import Header from "./components/Header/index";
 import Home from "./Pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate replace to="/login" />} />
           <Route path="/login" element={<LoginForm />} />
           <Route element={<WithHeader />}>
             <Route path="/home" element={<Home />} />
